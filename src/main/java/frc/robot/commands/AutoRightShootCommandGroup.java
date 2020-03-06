2f232489961791fefe2f372080d19cc45f2a5265
js-RAM <59541686+js-RAM@users.noreply.github.com>
2020-03-05 19:53:43 -0600

    working auto collect

--- src/main/java/frc/robot/commands/AutoRightShootCommandGroup.java
+++ src/main/java/frc/robot/commands/AutoRightShootCommandGroup.java
@@ -44,19 +44,18 @@ public class AutoRightShootCommandGroup extends SequentialCommandGroup {
         new DriveTurnCommand(22.48, driveTrain),
         new TestHighShootCommandGroup(0.5, 1.5, intake, leftShooter, rightShooter).withTimeout(3)
         
-        /*new DriveDistanceCommand(160, true, driveTrain)
-        .raceWith(new IntakeCommand(IntakeConstant.INTAKE_SPEED, 
-        IntakeConstant.CONVEYOR_SPEED, intake)),
-        new DriveDistanceCommand(-73.37, false, driveTrain),
-        new DriveTurnCommand(22.48, driveTrain),
-        //new VisionCommandGroup(driveTrain),
-        //new PidShootCommandGroup(3000, intake, leftShooter, rightShooter).withTimeout(6),
-        new TestHighShootCommandGroup(0.95, 2, intake, leftShooter, rightShooter).withTimeout(5),
-        new DriveTurnCommand(-22.48, driveTrain),
-        new DriveDistanceCommand(114, true, driveTrain)
-        .raceWith(new IntakeCommand(IntakeConstant.INTAKE_SPEED,
-        IntakeConstant.CONVEYOR_SPEED, intake))*/
-
+    /*new DriveDistanceCommand(160, true, driveTrain)
+    .raceWith(new IntakeCommand(IntakeConstant.INTAKE_SPEED, 
+    IntakeConstant.CONVEYOR_SPEED, intake)),
+    new DriveDistanceCommand(-73.37, false, driveTrain),
+    new DriveTurnCommand(22.48, driveTrain),
+    //new VisionCommandGroup(driveTrain),
+    //new PidShootCommandGroup(3000, intake, leftShooter, rightShooter).withTimeout(6),
+    new TestHighShootCommandGroup(0.95, 2, intake, leftShooter, rightShooter).withTimeout(5),
+    new DriveTurnCommand(-22.48, driveTrain),
+    new DriveDistanceCommand(114, true, driveTrain)
+    .raceWith(new IntakeCommand(IntakeConstant.INTAKE_SPEED,
+    IntakeConstant.CONVEYOR_SPEED, intake))*/
     );
   }
 }
